fcc1b03628e7ef736c1e1f8d3adb7175a47d739b
Fernando Tanaka <fernando.tomio.tanaka@gmail.com>
2023-02-22 22:09:33 -0300

    tratamento do input mensagem de erro navegação

--- app/src/main/java/br/com/impacta/curso/impacta2023_advinhanumero/InicialFragment.kt
+++ app/src/main/java/br/com/impacta/curso/impacta2023_advinhanumero/InicialFragment.kt
@@ -1,59 +1,52 @@
 package br.com.impacta.curso.impacta2023_advinhanumero
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import br.com.impacta.curso.impacta2023_advinhanumero.databinding.FragmentInicialBinding
+import kotlin.random.Random
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [InicialFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InicialFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentInicialBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicial, container, false)
+        _binding = FragmentInicialBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InicialFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InicialFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val numeroSorteado = Random.nextInt(0, 10)
+        Log.d("IMPACTA2", "numeroSorteado: $numeroSorteado")
+
+        binding.button.setOnClickListener {
+            try {
+                val inputUsuario = binding.editTextNumber.text.toString().toInt()
+
+                val acao = when {
+                    inputUsuario > numeroSorteado -> R.id.action_inicialFragment_to_chuteMaiorFragment
+                    inputUsuario < numeroSorteado -> R.id.action_inicialFragment_to_chuteMenorFragment
+                    else -> R.id.action_inicialFragment_to_parabensFragment
                 }
+
+                findNavController().navigate(acao)
+            } catch (e: java.lang.NumberFormatException) {
+                binding.editTextNumber.error = getString(R.string.nao_digitou_numero)
             }
+        }
     }
+
+
 }
